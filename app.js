16f77f5b0291ee22490f5772773011571ba535a1
mahdiktu <mahdiallafi1@gmail.com>
2021-01-26 13:54:15 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const bodyParser = require('body-parser')
+const date=require(__dirname+"/date.js")
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+const items=["eat","read","play"];
+app.get("/", function(req, res) {
+let day=date.getDay();
+  res.render("list", {kindday:day , newitems:items});
+});
+app.post("/",function(req,res){
+  let item=req.body.ev;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.get("/about",function(req,res){
+  res.render("about");
+});
+app.listen(3000, function() {
+  console.log("server is runnning om port 3000");
+})
