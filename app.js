cef019b81db2c6a6863c91f133c5791ac0a79981
mahdiallafi <72875842+mahdiallafi@users.noreply.github.com>
2021-01-26 13:58:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-const items=["eat","read","play"];
+
 app.get("/", function(req, res) {
 let day=date.getDay();
   res.render("list", {kindday:day , newitems:items});
